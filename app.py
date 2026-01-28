7667794938d0948cebe14c9a847f57f1f158ee57
krishna <krishna04.b@gmail.com>
2026-01-28 12:48:13 +0000

    python-code

---
+++ app.py
@@ -0,0 +1,11 @@
+eno="1001"
+name="krishna"
+course="DevOps"
+print("welcome to github actions")
+print(f'My Eno is: {eno}')
+print(f'My Name is: {name}')
+print(f'My Course is: {course}')
+print("====================")
+print("Learn Here lead any where")
+
+
